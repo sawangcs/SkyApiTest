2b7a6efcaf85e6e72876dfec9f193355f474ac91
Sawangchai Saroj <sawangcs@ais.co.th>
2022-10-27 13:35:52 +0700

    Add project files.

---
+++ SkyApiTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace SkyApiTest
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                RestClient client = new RestClient(new RestClientOptions("https://phxdev-nginx1.intra.ais:2443/"));
+                RestRequest request = new RestRequest("sky-auth/v1/user/authenticate");
+                request.AddHeader("Content-Type", "application/json");
+                request.AddStringBody("{ \"username\":\"oly_usr\", \"password\":\"ebb1345b837c3039d16e9c0675cc449a\"}", "application/json");
+                Console.WriteLine((await client.PostAsync(request)).Content!.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
